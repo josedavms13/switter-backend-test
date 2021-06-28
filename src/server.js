5147e8e263a8bd2e64b2d4865d62d324c593bc3c
josedavms13 <josedav0ms13@gmail.com>
2021-06-27 23:35:13 -0500

    Before refactor

--- src/server.js
+++ src/server.js
@@ -1,181 +1,40 @@
 import express, {json} from 'express'
+import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import fs from 'fs'
+import path from 'path'
+
+dotenv.config();
 
 const app = express();
 
-app.set('name', 'Switter backend prototype');
-app.set('port', process.env.port || 4550 )
-
-app.use( express.json() )
-
-const dogsObject = {
-    dogs : [{
-        id: 0,
-        name: 'Donatella',
-        owner: 'Ana Maria Piñeros',
-        state : 0,
-        recreations : {
-            price : 50000,
-            daysAmount : 0,
-            dates: [],
-        },
-        training : {
-            price : 50000,
-            daysAmount : 0,
-            dates: [],
-        },
-        kindergarten : {
-            isIn : false,
-            price : 50000,
-            daysAmount : 0,
-            dates: [],
-        },
-        extraItems : []
-    },
-        {
-            id: 1,
-            name: 'Malibu',
-            owner: 'Ana Maria Piñeros',
-            state : 0,
-            numOfRecreations : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            training : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            kindergarten : {
-                isIn : false,
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            addedItems : []
-        },
-        {
-            id: 2,
-            name: 'Bruno',
-            owner: 'Juan Felipe',
-            state : 0,
-            recreations : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            training : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            kindergarten : {
-                isIn : false,
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            addedItems : []
-        },
-        {
-            id: 3,
-            name: 'Abby',
-            owner: 'Juan Felipe',
-            state : 0,
-            recreations : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            training : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            kindergarten : {
-                isIn : false,
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            addedItems : []
-        },
-        {
-            id: 4,
-            name: 'Kayser',
-            owner: 'Juan Felipe',
-            state : 0,
-            recreations : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            training : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            kindergarten : {
-                isIn : false,
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            addedItems : []
-        },
-        {
-            id: 5,
-            name: 'Sacha',
-            owner: 'Juan Felipe',
-            state : 0,
-            recreations : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            training : {
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            kindergarten : {
-                isIn : false,
-                price : 50000,
-                daysAmount : 0,
-                dates: [],
-            },
-            addedItems : []
-        }]
-}
-
-app.get('/', ((req, res) => {
-    res.send('hello world');
-}))
-
-app.get('/dogs' , (req, res)=>{
-
-    res.json(dogsObject);
+const data = fs.readFile(path.resolve('./src/db/db.txt') , (error, data)=>{
+    if(error){
+        console.log(error)
+    }else{
+        return data.toString();
+    }
 })
 
 
-app.put('/dogs/:id' , ((req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    console.log(req.body);
-    console.log(Array.isArray(dogsObject.dogs))
-    const indexOfElement = dogsObject.dogs.findIndex(element => element.id === Number(id))
+app.get((req, res)=>{
+    console.log('get recieved')
+    console.log(req);
 
-    dogsObject.dogs[indexOfElement].kindergarten.isIn = req.body.isIn;
-    dogsObject.dogs[indexOfElement].addedItems.push(req.body.additionals);
-
-    console.log(dogsObject);
+    res.send('hello')
+    console.log(data);
 
 
-    res.json(dogsObject);
-}))
-
-
-app.listen(app.get('port'),()=>{
-    console.log('started');
 })
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT);
+console.log('listening on port ', process.env.PORT)
